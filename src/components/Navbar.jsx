7a67690d99a8626f1a54b8effcbbf29aa8a923c5
Satakshi <satakshisrivastava11@gmail.com>
2025-07-05 01:58:33 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiMenu, FiHeart, FiShoppingCart } from "react-icons/fi";
 import { NavLink } from "react-router-dom";
 import navbarlogo from "../assets/navbarlogo.png";
 
 const Navbar = () => {
+
+const [isMenuOpen, setIsMenuOpen]= useState(false);
   return (
     <nav className="w-full px-7 py-4 bg-white flex items-center justify-between shadow-md z-50 relative">
       
 
       <div className="flex items-center gap-6">
-        <FiMenu className="text-2xl cursor-pointer" />
+      <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
+     <FiMenu className="text-2xl cursor-pointer" />
+     </button>
 
         <div className="hidden md:flex gap-6 font-[Playfair] text-lg">
           <NavLink
@@ -85,6 +89,23 @@ const Navbar = () => {
         <FiHeart className="text-xl" />
         <FiShoppingCart className="text-xl" />
       </div>
+
+      {isMenuOpen && (
+  <div className="fixed top-0 left-0 w-64 h-full bg-white shadow-lg z-[100] transition-transform duration-300 ease-in-out p-6 flex flex-col gap-4">
+    <button className="self-end text-2xl" onClick={() => setIsMenuOpen(false)}>×</button>
+    
+    <NavLink to="/" onClick={() => setIsMenuOpen(false)} className="text-lg font-playfair">Home</NavLink>
+    <hr className="my-1" />
+    <NavLink to="/about" onClick={() => setIsMenuOpen(false)} className="text-lg font-playfair">About</NavLink>
+    <hr className="my-0" />
+    <NavLink to="/contact" onClick={() => setIsMenuOpen(false)} className="text-lg font-playfair">Contact</NavLink>
+    <hr className="my-0" />
+    <p className="text-sm font-semibold text-gray-500">Product Categories</p>
+    <NavLink to="/category/yarn" onClick={() => setIsMenuOpen(false)} className="text-md">Yarn</NavLink>
+    <NavLink to="/category/kits" onClick={() => setIsMenuOpen(false)} className="text-md">Crochet Kits</NavLink>
+    <NavLink to="/category/bags" onClick={() => setIsMenuOpen(false)} className="text-md">Handmade Bags</NavLink>
+  </div>
+)}
     </nav>
   );
 };

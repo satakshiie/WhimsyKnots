ca2a4e76a681ddd907250ccd3594b49bbc90ec8d
Satakshi <satakshisrivastava11@gmail.com>
2025-07-07 02:09:41 +0530

    in work

--- src/components/BestSellers.jsx
+++ src/components/BestSellers.jsx
@@ -4,6 +4,8 @@ import coaster from "../assets/coaster.png";
 import strawberry from "../assets/strawberry.png";
 import flower from "../assets/flower.png";
 import daisy from "../assets/daisy.png";
+import holder from "../assets/holder.png"
+import FadeInOnView from './FadeInOnView';
 
 
 const products = [
@@ -23,9 +25,14 @@ const products = [
     image: strawberry,
   },
   {
-    title: "Pink Coaster",
+    title: "Pink Detailed Coaster",
     price: "$150",
     image: coaster,
+  },
+  {
+    title: "Sunglass case",
+    price: "$150",
+    image: holder,
   },
 ];
 
@@ -41,20 +48,22 @@ const BestSellers = () => {
     const smoothX = useSpring(x, { stiffness: 100, damping: 30 });
   
     return (
-      <section className="w-full bg-[#FFFBDD] py-10 px-4 md:px-10">
-        <div className="text-center mb-10">
-          <h2 className="text-fontGreen font-[Playfair] text-4xl md:text-5xl">Our Best Sellers</h2>
+      <section className="w-full bg-[#FFFBDD] px-4 md:px-10">
+          
+          <div ref={ref} className="relative h-[1000vh] sm:h-[1000vh] md:h-[700vh] lg:h-[400vh]">
+          <div className="sticky top-0 h-screen flex flex-col items-center justify-center overflow-x-hidden">
+          <div className="text-center mb-12 -mt-6">
+        <FadeInOnView><h2 className="text-fontGreen font-[Playfair]  text-4xl md:text-5xl">Our Best Sellers</h2>
           <p className="text-lg font-[Playfair] md:text-xl mt-2 text-fontGreen">
             Little handcrafted joys people keep coming back for.
           </p>
+          </FadeInOnView>
         </div>
   
-        <div ref={ref}     className="relative h-[900vh] sm:h-[1000vh] md:h-[700vh] lg:h-[500vh]">
-          <div className="sticky top-0 h-screen flex items-center justify-center overflow-x-hidden">
-          <div className="bg-[#FEF6B7] rounded-[80px] py-10 px-4 sm:px-6 md:px-10 lg:px-16 xl:px-24 w-full max-w-[900px] xl:max-w-[1400px] mx-auto overflow-x-hidden">
+          <div className="bg-[#FEF6B7] rounded-[80px] py-15 px-4 sm:px-6 md:px-10 lg:px-14 xl:px-24 w-full max-w-[900px] xl:max-w-[1400px] mx-auto overflow-x-hidden">
               <motion.div style={{ x: smoothX }} className="flex gap-10 w-fit">
               {products.map((item, index) => {
-const cardCenter = index * 310 + 150; // each card width (300) + 10 gap
+const cardCenter = index * 510 + 150; 
 const containerCenter = window.innerWidth / 2;
 
 const distance = useTransform(smoothX, (latestX) => {
@@ -77,6 +86,7 @@ const distance = useTransform(smoothX, (latestX) => {
                         alt={item.title}
                         className="w-[200px] h-[250px] md:w-[250px] lg:w-[250px] object-cover  object-center rounded-t-2xl"
                       />
+      
                       <h3 className="text-lg font-[Playfair] mt-4 text-fontGreen">{item.title}</h3>
                       <p className="text-md text-gray-700 font-semibold">{item.price}</p>
                     </motion.div>

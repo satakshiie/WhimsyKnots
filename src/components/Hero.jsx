df1eb349ae52ecb78b84fd87f4010371fc648283
Satakshi <satakshisrivastava11@gmail.com>
2025-07-06 12:28:03 +0530

    in work

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,21 +5,18 @@ import { motion } from "framer-motion";
 const Hero = () => {
 
     return (
-        <section className="w-full bg-hero-gradient overflow-hidden  py-15 md:py-16">
+        <section className="w-full bg-hero-gradient overflow-hidden  py-15 md:py-10">
           <div className="flex flex-col md:flex-row items-center justify-between gap-4 sm:gap-6 md:gap-10 relative">
       
          {/* Left Content */}
         
-<div className="w-full  md:w-1/2 relative pl-4 pt-10 pb-15 sm:pl-10 md:pl-20 lg:pl-40 flex flex-col justify-center  text-center">
-
-
-{/* Text shifted slightly toward center */}
+<div className="w-full  md:w-1/2 relative pl-4 pt-10 pb-15 sm:pl-10 md:pl-20 lg:pl-40 flex flex-col justify-center  md:text-left text-center">
 <motion.div
   initial={{ opacity: 0, y: 40 }}
   whileInView={{ opacity: 1, y: 0 }}
   transition={{ duration: 1, ease: [0.25, 0.1, 0.25, 1] }}
   viewport={{ once: true }}
-  className="flex flex-col items-center justify-center text-center"
+  className="flex flex-col items-center justify-center text-center "
 >
 <h1 className="font-[Playfair] text-4xl md:text-5xl lg:text-7xl  text-fontGreen leading-tight">
   Whim
@@ -31,7 +28,7 @@ const Hero = () => {
   </div>
 </h1>
 
-  <h2 className="font-[Playball] text-2xl md:text-3xl   lg:text-5xl mt-2 md:mt-4 text-yarnGreen">A crochet affair</h2>
+  <h2 className="font-[Playball] text-2xl md:text-3xl   lg:text-5xl mt-2 md:mt-4 text-yarnGreen  ">A crochet affair</h2>
   <p className="font-[Playfair] text-l md:text-xl  lg:text-3xl mt-1 text-yarnGreen text-center">
   Every stitch a story—<br />handcrafted just for you. Dive in!
 </p>
@@ -54,7 +51,7 @@ whileInView={{ opacity: 1, y: 0, scale: 1 }}
               <img
                 src={BAG}
                 alt="Crochet Bag"
-                className="w-full max-w-[400px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[700px] xl:max-w-[700px]"
+                className="w-full max-w-[400px] sm:max-w-[300px] md:max-w-[400px] lg:max-w-[700px] xl:max-w-[750px]"
               />
             </motion.div>
           </div>
